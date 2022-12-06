f082e06d294d64972567f948aa17e3a6cfb954aa
Kashyab19 <kashyabnarayanan3721@gmail.com>
2022-12-05 23:32:26 -0500

    Finished classes

--- src/main/java/com/mycompany/datamarketplace/datamodels/community/Community.java
+++ src/main/java/com/mycompany/datamarketplace/datamodels/community/Community.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.datamarketplace.datamodels.community;
 
-/**
- *
- * @author kashyabmurali
- */
+package com.mycompany.datamarketplace.datamodels.community;
 public class Community {
-    
+    int communityId;
+    String communityName;
 }
