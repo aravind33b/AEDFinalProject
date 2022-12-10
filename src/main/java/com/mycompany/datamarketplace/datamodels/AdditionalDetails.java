f5836e5f816d88695ec70ff996ad0b28fd505c0e
Kashyab19 <kashyabnarayanan3721@gmail.com>
2022-12-09 22:05:21 -0500

    Completed some enterprises and roles

--- src/main/java/com/mycompany/datamarketplace/datamodels/AdditionalDetails.java
+++ src/main/java/com/mycompany/datamarketplace/datamodels/AdditionalDetails.java
@@ -10,8 +10,60 @@ package com.mycompany.datamarketplace.datamodels;
  */
 public class AdditionalDetails {
     String city;
+    String country;
     String state;
     String zipcode;
     String officialEmail;
     String officialPhone;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getOfficialEmail() {
+        return officialEmail;
+    }
+
+    public void setOfficialEmail(String officialEmail) {
+        this.officialEmail = officialEmail;
+    }
+
+    public String getOfficialPhone() {
+        return officialPhone;
+    }
+
+    public void setOfficialPhone(String officialPhone) {
+        this.officialPhone = officialPhone;
+    }
+    
+    
 }
--- src/main/java/com/mycompany/datamarketplace/datamodels/company/Company.java
+++ src/main/java/com/mycompany/datamarketplace/datamodels/company/Company.java
@@ -10,8 +10,32 @@ import com.mycompany.datamarketplace.datamodels.AdditionalDetails;
  *
  * @author kashyabmurali
  */
-public class Company {
+public class Company extends AdditionalDetails {
+    
     String companyName;
     String companyId;
-    AdditionalDetails additionalDetails;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+    
+    @Override
+    public String toString(){
+        return companyName;
+    }
+    
+    
+    
 }
